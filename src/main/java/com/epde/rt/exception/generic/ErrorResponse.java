f0dfb40e655553dc48265b48e358e4a11caa6a39
Md. Mosfikur Rahman <mosfikur.rahman@bjitgroup.com>
2023-03-14 12:43:08 +0600

    RandomThings: Task Management System - Updated Global Exception Handling - Implemented to work with Get with Title - Fixed bug at same name task shows conflict message.

--- src/main/java/com/epde/rt/exception/generic/ErrorResponse.java
+++ src/main/java/com/epde/rt/exception/generic/ErrorResponse.java
@@ -1,14 +1,9 @@
 package com.epde.rt.exception.generic;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class ErrorResponse {
     private int status;
     private String message;
